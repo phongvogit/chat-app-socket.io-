b18544b727d0348ee26faa8e5c89dbfd5649bb5e
Phong <v.ducphong@gmail.com>
2021-06-23 19:10:33 +0300

    Add automatic scrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,6 +16,31 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 //Options
 const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true})
 
+
+const autoScroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+    // Height of new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    //Visible Height
+    const visibleHeight = $messages.offsetHeight
+
+    //Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+    console.log('scrollOffset: ' + scrollOffset)
+    console.log('containerHeight: ' + containerHeight)
+
+    if(containerHeight - newMessageHeight <= scrollOffset){
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
 socket.on('message', (message) => {
     const html = Mustache.render(messageTemplate, {
         username: message.username,
@@ -23,6 +48,7 @@ socket.on('message', (message) => {
         createdAt: moment(message.createdAt).format('h:mm a') 
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoScroll()
 })
 
 socket.on('locationMessage', (location) => {
